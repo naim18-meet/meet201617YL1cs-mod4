3a545d19ee6e4fc9bd4b473286e4af1b4ed7343c
naim18-meet <naimjm@gmail.com>
2017-01-25 19:44:14 +0200

    DONE

--- square.py
+++ square.py
@@ -10,8 +10,31 @@ class Square (Rectangle):
         
     def set_length (self, new_length):
         super(Square, self).set_length(new_length)
-        new_length = self.length
-        new_height = self.length
+
+        if new_length>=0 :
+            self.length=new_length
+            self.height=new_length
+            if self.has_been_drawn : #Only redraw rectangle; don't make new drawing.
+                self.draw_shape()
+
+
+
+        #self.length = new_length
+        #self.height = new_length
+        
+
+    def set_height (self, new_height):
+        super(Square, self).set_height(new_height)
+
+        if new_height>=0 :
+            self.height=new_height
+            self.length=new_height
+            if self.has_been_drawn : #Only redraw rectangle; don't make new drawing.
+                self.draw_shape()
+
+
+        #self.height = new_height
+        #self.length = new_height
 
     def get_area(self):
     
